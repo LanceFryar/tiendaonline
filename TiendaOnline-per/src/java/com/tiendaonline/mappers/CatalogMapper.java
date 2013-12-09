b6e7b924a27ce401a37d4993c9a75433f06728e5
Josué <Josu@10.230.172.101>
2013-12-09 13:42:17 +0000

    Añadidas cosas de base de datos

--- TiendaOnline-per/src/java/com/tiendaonline/mappers/CatalogMapper.java
+++ TiendaOnline-per/src/java/com/tiendaonline/mappers/CatalogMapper.java
@@ -8,6 +8,8 @@ package com.tiendaonline.mappers;
 
 import com.tiendaonline.interfacebeans.ICatalog;
 import com.tiendaonline.libraries.IProduct;
+import com.tiendaonline.model.DomainObject;
+import java.sql.ResultSet;
 import javax.ejb.Stateless;
 
 /**
@@ -15,10 +17,20 @@ import javax.ejb.Stateless;
  * @author Josué
  */
 @Stateless
-public class CatalogMapper implements ICatalog {
+public class CatalogMapper extends AbstractMapper implements ICatalog {
 
     @Override
     public IProduct getProduct(String id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected String findStatement() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    protected DomainObject doLoad(Long id, ResultSet rs) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
